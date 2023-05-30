2a9b5335a00226e6089855ed6f961dd835295991
Roman <romankorovin032@gmail.com>
2023-05-30 14:22:01 +0300

    сделана регистрация через гитхаб но криво на доделать

--- src/main/java/com/freeCinema/cinema/controllers/HomeController.java
+++ src/main/java/com/freeCinema/cinema/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.freeCinema.cinema.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
---
+++ src/main/java/com/freeCinema/cinema/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.freeCinema.cinema.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/secured")
+    public String Great() {
+        return "great";
+    }
+}
