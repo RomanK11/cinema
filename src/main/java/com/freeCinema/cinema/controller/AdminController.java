d7f08990d80b63e6218f68ba6e4c6644d77a6479
Roman <romankorovin032@gmail.com>
2023-10-21 22:18:18 +0300

    Добавление фильмов каряво работает

--- src/main/java/com/freeCinema/cinema/controller/AdminController.java
+++ src/main/java/com/freeCinema/cinema/controller/AdminController.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.freeCinema.cinema.model.FilmPage;
+import com.freeCinema.cinema.model.FilmPreview;
 import com.freeCinema.cinema.model.User;
+import com.freeCinema.cinema.repository.FilmPageRepository;
+import com.freeCinema.cinema.repository.FilmPreviewRepository;
 import com.freeCinema.cinema.repository.UserRepository;
+import com.freeCinema.cinema.service.InternalServerException;
 
 @Controller
 @RequestMapping(path = "/admin")
@@ -16,11 +23,35 @@ public class AdminController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private FilmPreviewRepository filmPreviewRepository;
+
+    public boolean existsByField(String fieldValue1, String fieldValue2, String fieldValue3) {
+        return filmPreviewRepository.searchFilmsByAllArgs(fieldValue1, fieldValue2, fieldValue3) != null;
+    }
+
     @GetMapping("/users")
-    public String getAdminPanel(Model model) {
+    public String getUsers(Model model) {
         Iterable<User> users = userRepository.findAll();
         model.addAttribute("users", users);
         return "adminUsers";
     }
 
+    @GetMapping("/add-film")
+    public String getFilms(Model film) {
+        Iterable<FilmPreview> films = filmPreviewRepository.findAll();
+        film.addAttribute("films", films);
+        return "adminFilms";
+    }
+
+    @PostMapping(path = "/add-film")
+    public String handleAddForm(@ModelAttribute("film") FilmPreview film) {
+        filmPreviewRepository.save(film);
+        return "jj";
+    }
+
+    @ModelAttribute(name = "film")
+    public FilmPreview film() {
+        return new FilmPreview();
+    }
 }
--- src/main/java/com/freeCinema/cinema/repository/FilmPreviewRepository.java
+++ src/main/java/com/freeCinema/cinema/repository/FilmPreviewRepository.java
@@ -9,4 +9,8 @@ import com.freeCinema.cinema.model.FilmPreview;
 public interface FilmPreviewRepository extends CrudRepository<FilmPreview, Long> {
     @Query("SELECT * FROM FILMPREVIEW WHERE TITLE = :title")
     Iterable<FilmPreview> searchFilms(@Param("title") String title);
+
+    @Query("SELECT * FROM filmpreview WHERE TITLE = :title AND PICTURE = :picture AND THEME = :theme")
+    Iterable<FilmPreview> searchFilmsByAllArgs(@Param("title") String title, @Param("picture") String picture,
+            @Param("theme") String theme);
 }
